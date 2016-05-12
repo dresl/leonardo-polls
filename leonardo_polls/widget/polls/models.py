07988699f89a59fa5dc80964cf4a50166199882d
dresl <nikicresl@gmail.com>
2016-05-12 21:40:55 +0200

    update

--- leonardo_polls/widget/polls/models.py
+++ leonardo_polls/widget/polls/models.py
@@ -9,8 +9,7 @@ class PollsWidget(Widget):
 
 	poll = models.ForeignKey('Poll', verbose_name='Poll')
 
-    class Meta:
+	class Meta:
         abstract = True
         verbose_name = _('Poll')
         verbose_name_plural = _('Polls')
-        
